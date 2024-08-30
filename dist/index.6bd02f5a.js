42f33ea9de9e6c1f14a46effbf18ea7541970d34
ankith7777 <ankithacharya5@gmail.com>
2024-08-30 13:23:21 +0530

    inginition

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,9 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, React.createElement("h1", {}, "i am an h1 tag")));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
